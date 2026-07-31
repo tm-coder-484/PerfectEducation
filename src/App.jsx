b59fea4ed511ebfba105a094eb2889625a2bdb63
Lil Jim <tmaconachie@icloud.com>
2026-07-31 00:38:30 +0000

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Bar from "./Bar.jsx";
 import Content from "./Content.jsx";
+import About from "./About.jsx";
+import Apply from "./Apply.jsx";
+
+import { Routes, Route, Link } from 'react-router'
 
 function App() {
   return (
     <>
       <div className="main">
         <Bar />
-        <Content />
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/apply" element={<Apply />} />
+        </Routes>
+        
       </div>
     </>
   );
