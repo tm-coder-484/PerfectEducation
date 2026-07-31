8fe3b02dcc59a3e9e8d2d22c3174e2677c47638c
MACONACHIE <tmaco0@eq.edu.au>
2026-07-31 15:39:06 +1000

    Add some more stuff including setup for the lessons page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Content />} />
           <Route path="/about" element={<About />} />
           <Route path="/apply" element={<Apply />} />
+          <Route path="/lessons" element={<Lessons />} />
         </Routes>
         
       </div>
