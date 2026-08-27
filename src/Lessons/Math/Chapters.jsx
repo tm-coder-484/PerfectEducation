ddd78efb5245a41cf461d4832d67c6cbd1234710
tm-coder-484 <228451049+tm-coder-484@users.noreply.github.com>
2026-08-27 16:11:03 +1000

    idk

--- src/Lessons/Math/Chapters.jsx
+++ src/Lessons/Math/Chapters.jsx
@@ -2,7 +2,7 @@ import { LessonSidebarButton } from '../../Button.jsx'
 import { InlineMath, BlockMath } from 'react-katex';
 import {MathRenderer} from '../../MathRenderer.jsx'
 
-export function M1_1() {
+export function Algebra() {
   return (
     <>
       <h1>Introduction to algebra</h1>
@@ -19,8 +19,27 @@ export function M1_1() {
         
         ## Like and unlike terms
         You cannot compare apples to oranges. The same goes for like and unlike terms. Like terms are expressions that share a common variable and can be simplified when adding. For example $3x$ and $5x$ are like terms because they share a common variable(x), and adding them gives $3x+5x=8x$. In contrast, $2x$ and $8y$ are not like terms, this is because they have different variables attached(x and y) and therefore cannot be added.
+        ## Solving basic equations
+        To solve algebraic equations, you must isolate your variable by adding, subtracting, multiplying or dividing numbers on both sides. You can think of this as keeping a see saw balanced. For example to solve $x+4=10$, you first subtract 4 from both sides, as in order to get rif of the +4 we must take it away, so we get $x=10-4$ or $x=6$. For subtraction, we add values to both sides to cancel out the subtraction. For example to solve $x-1=2$, add 1 to both sides to get $x=2+1$ or $x=3$. For multipication, you divide on both sides to cancel out the multiplication, for example to solve $2x=4$, you divide by two on both sides to get $x=4/2$ or $x=2$. For divison, multiply on both sides. For example to solve $x/4=2$, you multiply by 4 on both sides to get $x=4\cdot 2$ or $x=8$. 
+        ## Verifying solutions
+        To verify your solutions to equations, you can simply replace all values of x with the number you solved x to be equal to and check if the left hand side equals the right hand side. To verify $x=4$ as the solution to $3x=12$, replace the x with 4(as that is the solution we found) to get $3 \cdot 4=12$, and simplifying further we got $12=12$, verifying our solution is correct.
+        ## Two step equations 
+        Two step equations combine two operations into one equation with a variable, adding an extra layer of complexity. Examples include $2x+4=10$ and $3x-2=13$. To solve the equation, you have to apply two of our two previous steps to isolate x. To solve $2x+4=10$, we first subtract 4 from both sides to get $2x=6$, now, apply our second step of dividing by 2 in order to fully isolate x and get our result of $x=3$
         `} />
 
+    </>
+  )
+}
+
+export function Trigonometry() {
+  return (
+    <>
+      <h1>Introduction to trigonometry</h1>
+      <MathRenderer content={String.raw`
+        Trigonometry is a branch of math that deals with the relationships between the sides and angles of triangles. We can use it to solve for unknown sides and angles in a triangle making it very useful. 
+        ## Trigonometric ratios
+        The three quintessential trigonometry ratios are sine, cosine and tangent. They are equal to the ratio of two sides of a right angled triangle. 
+      `} />
     </>
   )
 }
